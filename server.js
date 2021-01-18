f3f140ce3676a4d693ac788470501ba648faf798
Aditya-011 <adihgr18@gmail.com>
2021-01-19 00:48:40 +0530

    created server and boiler plate

---
+++ server.js
@@ -0,0 +1,20 @@
+//imports
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const dotenv = require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+//server
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}...`);
+});
+//set template engines as ejs
+app.set("view engine", "ejs");
+//app.use(expressLayout);
+app.set("views", path.join(__dirname, "/views"));
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
