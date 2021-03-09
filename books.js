cfb9ac07a231918393aab35f16250d84e0b79f9a
Aditya-011 <adihgr18@gmail.com>
2021-03-09 22:55:30 +0530

    basic layout tweaks

---
+++ books.js
@@ -0,0 +1,25 @@
+const books = [
+  { name: "A Promised Land", author: "Barack Obama", price: 1999 },
+  { name: "The Battle of Belonging", author: "Shashi Tharoor", price: 799 },
+  { name: "Portraits of Power", author: "N.K. Singh", price: 595 },
+  { name: "Remnants of a Separation", author: " Aanchal Malhotra", price: 599 },
+  { name: "The India Way", author: "S. Jaishankar", price: 699 },
+  {
+    name: "Jinnah: His Successes, Failures and Role in History",
+    author: " Ishtiaq Ahmed",
+    price: 999,
+  },
+  {
+    name: "Red Fear: The China Threat",
+    author: " Iqbal Chand Malhotra",
+    price: 799,
+  },
+  { name: "One Arranged Murder", author: " Chetan Bhagat", price: 123 },
+  {
+    name: "The Immortals of Melhua(Shiva Trilogy):1",
+    author: " Amish",
+    price: 236,
+  },
+  { name: "Roads to Mussoorie", author: " Ruskin Bond", price: 138 },
+];
+exports.books = books;
