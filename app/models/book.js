b396cf0da2b1bc5c428bbd094e2cf5156281c176
Aditya-011 <adihgr18@gmail.com>
2021-06-16 19:38:04 +0530

    detail route removed

--- app/models/book.js
+++ app/models/book.js
@@ -6,6 +6,7 @@ const bookSchema = new Schema({
   image: { type: String, required: true },
   price: { type: Number, required: true },
   author: { type: String, required: true },
+  description: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Book", bookSchema);
